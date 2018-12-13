d1d552d8f5b6d4f23bac75f6c42576e4306f2b31
ronething <13414113153@163.com>
2018-12-13 23:19:21 +0800

    核心功能封装为 MusicVisualizer 对象🎵

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -3,6 +3,21 @@ function $(s) {
 }
 
 var music_list = $("#list li");
+var size = 128;
+
+var box = $("#box")[0];
+var height, width;
+var canvas = document.createElement("canvas");
+var ctx = canvas.getContext("2d");
+var types = $("#type li");
+var Dots = [];
+var line;
+var mv = new MusicVisualizer({
+    size: size,
+    visualizer: draw,
+});
+
+box.appendChild(canvas);
 
 for (var i = 0; i < music_list.length; i++) {
     music_list[i].onclick = function () {
@@ -10,11 +25,10 @@ for (var i = 0; i < music_list.length; i++) {
             music_list[j].className = "";
         }
         this.className = "selected";
-        load("/media/" + this.title);
+        // load("/media/" + this.title);
+        mv.play("/media/" + this.title);
     }
 }
-
-var types = $("#type li");
 
 for (var i = 0; i < types.length; i++) {
     types[i].onclick = function () {
@@ -26,46 +40,24 @@ for (var i = 0; i < types.length; i++) {
     }
 }
 
-var xhr = new XMLHttpRequest();
-var ac = new (window.AudioContext || window.webkitAudioContext)();
-var gainNode = ac[ac.createGain ? "createGain" : "createGainNode"]();
-gainNode.connect(ac.destination);
-
-var analyser = ac.createAnalyser();
-var size = 128;
-analyser.fftSize = size * 2;
-analyser.connect(gainNode);
-
-var source = null;
-var count = 0;
-
-var box = $("#box")[0];
-var height, width;
-var canvas = document.createElement("canvas");
-var ctx = canvas.getContext("2d");
-box.appendChild(canvas);
-
-var Dots = [];
-
-function random(m,n) {
-    return Math.round(Math.random()*(n-m)+m);
+function random(m, n) {
+    return Math.round(Math.random() * (n - m) + m);
 }
 
 function getDots() {
     Dots = [];
-    for(var i = 0;i<size;i++){
-        var x = random(0,width);
-        var y = random(0,height);
-        var color = "rgb("+random(0,255)+","+random(0,255)+","+random(0,255)+")";
+    for (var i = 0; i < size; i++) {
+        var x = random(0, width);
+        var y = random(0, height);
+        var color = "rgb(" + random(0, 255) + "," + random(0, 255) + "," + random(0, 255) + ")";
         Dots.push({
-            x:x,
-            y:y,
+            x: x,
+            y: y,
             color: color
         });
     }
 }
 
-var line;
 function resize() {
     height = box.clientHeight;
     width = box.clientWidth;
@@ -87,17 +79,17 @@ function draw(arr) {
     var w = width / size;
     ctx.fillStyle = line;
     for (var i = 0; i < size; i++) {
-        if(draw.type=="column"){
+        if (draw.type == "column") {
             var h = arr[i] / 256 * height;
             ctx.fillRect(w * i, height - h, w * 0.8, h);
-        }else if(draw.type=="dot"){
+        } else if (draw.type == "dot") {
             ctx.beginPath();
             var o = Dots[i];
-            var r = arr[i]/256 * 50;
-            ctx.arc(o.x,o.y,r,0,Math.PI*2,true);
-            var g = ctx.createRadialGradient(o.x,o.y,0,o.x,o.y,r);
-            g.addColorStop(0,"#fff");
-            g.addColorStop(1,o.color);
+            var r = arr[i] / 256 * 50;
+            ctx.arc(o.x, o.y, r, 0, Math.PI * 2, true);
+            var g = ctx.createRadialGradient(o.x, o.y, 0, o.x, o.y, r);
+            g.addColorStop(0, "#fff");
+            g.addColorStop(1, o.color);
             ctx.fillStyle = g;
             ctx.fill();
             // ctx.strokeStyle = "#fff";
@@ -105,59 +97,11 @@ function draw(arr) {
         }
     }
 }
+
 draw.type = "column";
 
-function load(url) {
-    count++;
-    var n = count;
-    // 相当于 
-    // var n = ++count;
-    // console.log("n is ", n);
-    // console.log("count is ", count);
-    source && source[source.stop ? "stop" : "noteOff"]();
-    xhr.abort();
-    xhr.open("GET", url);
-    xhr.responseType = "arraybuffer";
-    xhr.onload = function () {
-        if (n != count) return;
-        ac.decodeAudioData(xhr.response, function (buffer) {
-            if (n != count) return;
-            var bufferSource = ac.createBufferSource();
-            bufferSource.buffer = buffer;
-            bufferSource.connect(analyser);
-            // bufferSource.connect(gainNode);
-            // bufferSource.connect(ac.destination);
-            bufferSource[bufferSource.start ? "start" : "noteOn"](0);
-            source = bufferSource;
-        }, function (err) {
-            console.log(err);
-        })
-    }
-    xhr.send();
-}
-
-function visualizer() {
-    var arr = new Uint8Array(analyser.frequencyBinCount);
-    var requestAnimationFrame = window.requestAnimationFrame ||
-        window.webkitRequestAnimationFrame ||
-        window.oRequestAnimationFrame ||
-        window.mzRequestAnimationFrame;
-    function v() {
-        analyser.getByteFrequencyData(arr);
-        draw(arr);
-        requestAnimationFrame(v);
-    }
-    requestAnimationFrame(v);
-}
-
-visualizer();
-
-function changeVolume(percent) {
-    gainNode.gain.value = percent * percent;
-}
-
 $("#volume")[0].onchange = function () {
-    changeVolume(this.value / this.max);
+    mv.changeVolume(this.value / this.max);
 }
 
 $("#volume")[0].onchange();
